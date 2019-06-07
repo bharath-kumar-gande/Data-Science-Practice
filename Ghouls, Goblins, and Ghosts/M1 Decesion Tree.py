d0d16da55ed8df4909d113e23fcc7d6842ad57db
bharath0508 <50160636+bharath0508@users.noreply.github.com>
2019-06-07 15:11:56 +0530

    Update M1 Decesion Tree.py

--- Ghouls, Goblins, and Ghosts/M1 Decesion Tree.py
+++ Ghouls, Goblins, and Ghosts/M1 Decesion Tree.py
@@ -38,15 +38,28 @@ X_train = g_total2[0:g_train1.shape[0]]
 y_train = g_train1['type1']
 
 tree_estimator = tree.DecisionTreeClassifier()
-tree_estimator.fit(X_train,y_train)
+dt_grid = {'max_depth':list(range(10,15)), 'min_samples_split':list(range(2,8)), 'criterion':['gini','entropy']}
 
-cv_scores2 = model_selection.cross_val_score(tree_estimator, X_train, y_train, cv=10)
-print(cv_scores2)
+param_grid = model_selection.GridSearchCV(tree_estimator, dt_grid, cv=10) #Evolution of tee
+param_grid.fit(X_train, y_train)
+# =============================================================================
+# 
+# # =============================================================================
+# # tree_estimator.fit(X_train,y_train)
+# # 
+# # =============================================================================
+# cv_scores2 = model_selection.cross_val_score(tree_estimator, X_train, y_train, cv=10)
+# print(cv_scores2)
+# =============================================================================
+print(param_grid.best_score_) #Best score
+print(param_grid.best_params_)
+print(param_grid.score(X_train, y_train)) #Train score  #Evalution of tree
+
 
 X_test = g_total2[g_train1.shape[0]:]
 X_test.info()
 
-g_test['type1'] = tree_estimator.predict(X_test)
+g_test['type1'] = param_grid.predict(X_test)
 g_test.info()
 
 def conv_type1(type):
@@ -60,6 +73,6 @@ def conv_type1(type):
 
 g_test.info()
 g_test.drop('type1',axis=1,inplace=True)
-
-os.chdir('/Users/bharath/Desktop/Data Science/Projects/house-prices-advanced-regression-techniques')
-g_test.to_csv('submission1.csv', columns=['id','type1'],index=False)
+g_test.info()
+os.chdir('/Users/bharath/Desktop/Data Science/Projects/ghouls-goblins-and-ghosts-boo')
+g_test.to_csv('submission1.csv', columns=['id','type'],index=False)
